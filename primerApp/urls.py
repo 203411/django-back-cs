7da3f0cb45c9dcd6f4ab15ace49c175ed01d0bbe
Marisol <203411@ids.upchiapas.edu.mx>
2022-01-19 14:53:26 -0600

    create models, serializers, views and urls

--- primerApp/urls.py
+++ primerApp/urls.py
@@ -1,7 +1,8 @@
 
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
+
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -22,5 +23,8 @@ router.register(r'users', UserViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
+    re_path(r'^api/v1/', include('primerComponente.urls')), 
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    
+    
 ]
