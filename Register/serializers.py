7d9e305f9100dc8f26bfb3e25484bfc1082e2efc
Marisol <203411@ids.upchiapas.edu.mx>
2022-01-23 14:38:50 -0600

    Indentation fixes in serializers.py

--- Register/serializers.py
+++ Register/serializers.py
@@ -4,28 +4,26 @@ from django.contrib.auth.models import User
 
 # Creacion de la clase para el registro
 class RegisterSerializer(serializers.Serializer):
-    id = serializers.ReadOnlyField()
     first_name = serializers.CharField()
     last_name = serializers.CharField()
     username = serializers.CharField()
     email = serializers.EmailField()
     password = serializers.CharField()
     
-# Metodo para crear un usuario
-def create(self, validate_data):
-    instance = User()
-    instance.first_name = validate_data.get('first_name')
-    instance.last_name = validate_data.get('last_name')
-    instance.username = validate_data.get('username')
-    instance.email = validate_data.get('email')
-    instance.set_password(validate_data.get('password'))
-    instance.save()
-    
-    return instance
+    # Metodo para crear un usuario
+    def create(self, validate_data):
+        instance = User()
+        instance.first_name = validate_data.get('first_name')
+        instance.last_name = validate_data.get('last_name')
+        instance.username = validate_data.get('username')
+        instance.email = validate_data.get('email')
+        instance.set_password(validate_data.get('password'))
+        instance.save()
+        return instance
 
-def validate_username(self, data):
-    users = User.objects.filter(username = data)
-    if (len(users) != 0):
-        raise serializers.ValidationError("Este nombre de usuario ya existe, ingresa uno nuevo")
-    else:
-        return data
+    def validate_username(self, data):
+        users = User.objects.filter(username = data)
+        if (len(users) != 0):
+            raise serializers.ValidationError("Este nombre de usuario ya existe, ingresa uno nuevo")
+        else:
+            return data
