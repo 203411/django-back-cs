74c90417f4a097c3402b63414de5cdd2ec018291
Marisol <203411@ids.upchiapas.edu.mx>
2022-02-23 14:36:24 -0600

    Correction to the Register

--- Register/views.py
+++ Register/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
 
 # Importaciones
-from rest_framework.response import Response
-from Register.serializers import RegisterSerializer
 from rest_framework.views import APIView
-from rest_framework import status
+from rest_framework import generics
 
 # Create your views here.
 
-class RegisterAPI(APIView):
-    def post(self, request):
-        serializer = RegisterSerializer(data = request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+class RegisterAPI(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+
+#     def post(self, request):
+#         serializer = RegisterSerializer(data = request.data)
+#         if serializer.is_valid():
+#             user = serializer.save()
+#             return Response(serializer.data, status = status.HTTP_201_CREATED)
+#         else:
+#             return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+        
         
